40146512832ded2feb79e3cec9886c802ba72913
kumarsoubackup-sys <kumarsou.backup@gmail.com>
2026-02-27 17:51:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1305,3 +1305,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print(f"🚀 Server running → http://localhost:{port}")
     app.run(host="0.0.0.0", port=port, debug=True)
+
